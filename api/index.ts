44e898890f0fcec0e2cbf3efe4b343523ad38484
beardofdoom <marcin.jusiak@gmail.com>
2022-12-13 18:41:14 +0100

    Adding components for testing

---
+++ api/index.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { TPost } from "../pages";
+
+const BASE_URL = `https://jsonplaceholder.typicode.com`;
+const getPosts = async (): Promise<TPost[]> => {
+  try {
+    const res = await axios.get(`${BASE_URL}/posts`);
+
+    return res.data.slice(90);
+  } catch (e) {
+    throw e;
+  }
+};
+
+const createPost = async (payload: TPost): Promise<TPost> => {
+  try {
+    const res = await axios.post(`${BASE_URL}/posts`, payload, {
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+};
+
+const updatePost = async ({ body, id }: { body: string; id: number }) => {
+  try {
+    const res = await axios.put(`${BASE_URL}/posts/${id}`);
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+};
+
+const deletePost = async (id: number) => {
+  try {
+    const res = await axios.delete(`${BASE_URL}/posts/${id}`);
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+};
+
+export { getPosts, createPost, updatePost, deletePost };
---
+++ components/index.ts
@@ -0,0 +1,4 @@
+import Post from "./post";
+import Button from "./button";
+import ExpandableTextarea from "./expandable-textarea";
+export { Post, Button, ExpandableTextarea };
